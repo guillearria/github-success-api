5c9bf0d51eb6ade1fbabf148d9a593a8f44cfe21
sapinspys <garri009@fiu.edu>
2020-06-17 21:56:02 -0400

    Connected endpoints to API

--- src/api/app.py
+++ src/api/app.py
@@ -79,47 +79,9 @@ class PullList(Resource):
         return [[pull.repo_id, pull.created_date] for pull in query], 200
 
 api.add_resource(Refresh, '/api/refresh')
-api.add_resource()
-api.add_resource()
+api.add_resource(RepoList, '/api/repo/all')
+api.add_resource(PullList, '/api/pull/<repo_name>')
 
 if __name__ == '__main__':
     # DISABLE DEBUG FOR PRODUCTION
     app.run(debug=True)
-
-# # Todo
-# # shows a single todo item and lets you delete a todo item
-# class Todo(Resource):
-#     def get(self, todo_id):
-#         # abort_if_todo_doesnt_exist(todo_id)
-#         return TODOS[todo_id]
-
-#     def delete(self, todo_id):
-#         abort_if_todo_doesnt_exist(todo_id)
-#         del TODOS[todo_id]
-#         return '', 204
-
-#     def put(self, todo_id):
-#         args = parser.parse_args()
-#         task = {'task': args['task']}
-#         TODOS[todo_id] = task
-#         return task, 201
-
-
-# # TodoList
-# # shows a list of all todos, and lets you POST to add new tasks
-# class TodoList(Resource):
-#     def get(self):
-#         return TODOS
-
-#     def post(self):
-#         args = parser.parse_args()
-#         todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-#         todo_id = 'todo%i' % todo_id
-#         TODOS[todo_id] = {'task': args['task']}
-#         return TODOS[todo_id], 201
-
-# ##
-# ## Actually setup the Api resource routing here
-# ##
-# api.add_resource(TodoList, '/todos')
-# api.add_resource(Todo, '/todos/<todo_id>')
