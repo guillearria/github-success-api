4aa7032271df3f6cc482a4dd04255a802ffa55d1
sapinspys <garri009@fiu.edu>
2020-06-20 05:31:29 -0400

    Abstracted seed logic to API manage file

--- services/api/manage.py
+++ services/api/manage.py
@@ -5,11 +5,56 @@ from project import app, db
 
 cli = FlaskGroup(app)
 
+
 @cli.command("create_db")
 def create_db():
     # db.connect()
     db.drop_tables([db.Repo, db.Pull])
     db.create_tables([db.Repo, db.Pull])
+
+
+@cli.command("seed_db")
+def seed_db():
+    temp_repos = ["kubernetes/kubernetes", "apache/spark"]
+    g = Github("d38d9579d63a4781c295fc499497f5d65714c2ed")
+    repos = [g.get_repo(repo) for repo in temp_repos]
+    pulls_paginatedLists = dict(
+        (repo.name, repo.get_pulls(state="all")) for repo in repos)
+
+    limit = date.today() - timedelta(3)
+    repo_names = list(pulls_paginatedLists.keys())
+    pulls_pls_3day = dict((name, []) for name in repo_names)
+
+    for repo in repo_names:
+        for pull in pulls_paginatedLists[repo]:
+            pull_date = pull.created_at.date()
+            if pull_date == date.today():
+                continue
+            elif pull_date >= limit:
+                pulls_pls_3day[repo].append(pull)
+            else:
+                break
+
+    repo_data = [{"name": name} for name in repo_names]
+
+    with db.db.atomic():
+        db.Repo.insert_many(repo_data).execute()
+
+    pulls_data = []
+
+    for repo in repo_names:
+        pulls = pulls_pls_3day[repo]
+        for pull in pulls:
+            pulls_data.append({"repo_id": db.Repo.get(db.Repo.name == repo),
+                               "created_date": pull.created_at.date(),
+                               "is_merged": pull.merged,
+                               "additions": pull.additions,
+                               "deletions": pull.deletions
+                               })
+
+    with db.db.atomic():
+        db.Pull.insert_many(pulls_data).execute()
+
 
 if __name__ == "__main__":
     cli()
--- services/api/project/__init__.py
+++ services/api/project/__init__.py
@@ -11,7 +11,7 @@ api = Api(app)
 #     if todo_id not in TODOS:
 #         abort(404, message="Todo {} doesn't exist".format(todo_id))
 
-temp_repos = ["kubernetes/kubernetes", "apache/spark"]
+# temp_repos = ["kubernetes/kubernetes", "apache/spark"]
 
 # top_repos = ["kubernetes/kubernetes", "apache/spark", "Microsoft/vscode",
 #              "nodejs/node", "tensorflow/tensorflow", "freeCodeCamp/freeCodeCamp",
@@ -19,51 +19,51 @@ temp_repos = ["kubernetes/kubernetes", "apache/spark"]
 
 # Refresh:
 # Clears DB and saves last 3 days of data for given repos
-class Refresh(Resource):
-    def post(self):
-        db.Repo.delete().execute()
-        db.Pull.delete().execute()
+# class Refresh(Resource):
+#     def post(self):
+#         db.Repo.delete().execute()
+#         db.Pull.delete().execute()
 
-        g = Github("d38d9579d63a4781c295fc499497f5d65714c2ed")
-        repos = [g.get_repo(repo) for repo in temp_repos]
-        pulls_paginatedLists = dict(
-            (repo.name, repo.get_pulls(state="all")) for repo in repos)
+#         g = Github("d38d9579d63a4781c295fc499497f5d65714c2ed")
+#         repos = [g.get_repo(repo) for repo in temp_repos]
+#         pulls_paginatedLists = dict(
+#             (repo.name, repo.get_pulls(state="all")) for repo in repos)
 
-        limit = date.today() - timedelta(3)
-        repo_names = list(pulls_paginatedLists.keys())
-        pulls_pls_3day = dict((name, []) for name in repo_names)
+#         limit = date.today() - timedelta(3)
+#         repo_names = list(pulls_paginatedLists.keys())
+#         pulls_pls_3day = dict((name, []) for name in repo_names)
 
-        for repo in repo_names:
-            for pull in pulls_paginatedLists[repo]:
-                pull_date = pull.created_at.date()
-                if pull_date == date.today():
-                    continue
-                elif pull_date >= limit:
-                    pulls_pls_3day[repo].append(pull)
-                else:
-                    break
+#         for repo in repo_names:
+#             for pull in pulls_paginatedLists[repo]:
+#                 pull_date = pull.created_at.date()
+#                 if pull_date == date.today():
+#                     continue
+#                 elif pull_date >= limit:
+#                     pulls_pls_3day[repo].append(pull)
+#                 else:
+#                     break
 
-        repo_data = [{"name": name} for name in repo_names]
+#         repo_data = [{"name": name} for name in repo_names]
 
-        with db.db.atomic():
-            db.Repo.insert_many(repo_data).execute()
+#         with db.db.atomic():
+#             db.Repo.insert_many(repo_data).execute()
 
-        pulls_data = []
+#         pulls_data = []
 
-        for repo in repo_names:
-            pulls = pulls_pls_3day[repo]
-            for pull in pulls:
-                pulls_data.append({"repo_id": db.Repo.get(db.Repo.name == repo),
-                                   "created_date": pull.created_at.date(),
-                                   "is_merged": pull.merged,
-                                   "additions": pull.additions,
-                                   "deletions": pull.deletions
-                                   })
+#         for repo in repo_names:
+#             pulls = pulls_pls_3day[repo]
+#             for pull in pulls:
+#                 pulls_data.append({"repo_id": db.Repo.get(db.Repo.name == repo),
+#                                    "created_date": pull.created_at.date(),
+#                                    "is_merged": pull.merged,
+#                                    "additions": pull.additions,
+#                                    "deletions": pull.deletions
+#                                    })
 
-        with db.db.atomic():
-            db.Pull.insert_many(pulls_data).execute()
+#         with db.db.atomic():
+#             db.Pull.insert_many(pulls_data).execute()
 
-        return {"message": "DB Refreshed"}, 201
+#         return {"message": "DB Refreshed"}, 201
 
 # RepoList
 # shows a list of all repos
