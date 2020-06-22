f080b01e9b9901748efb8b9bcd3f177ab160dd78
sapinspys <garri009@fiu.edu>
2020-06-21 21:40:22 -0400

    Updated files to meet PEP8

--- services/api/project/db.py
+++ services/api/project/db.py
@@ -4,23 +4,28 @@ from playhouse.shortcuts import model_to_dict
 
 db = PostgresqlDatabase(
     os.getenv("DATABASE"),
-    user=os.getenv("POSTGRES_USER"), 
+    user=os.getenv("POSTGRES_USER"),
     password=os.getenv("POSTGRES_PASSWORD"),
     host=os.getenv("SQL_HOST"), 
     port=os.getenv("SQL_PORT")
 )
 
 class BaseModel(Model):
+
+    
     class Meta:
         database = db
+
 
 # Table Description
 class Repo(BaseModel):
     name = CharField(255)
 
+
     def serialize(self):
         repo_dict = model_to_dict(self)
         return repo_dict
+
 
 class Pull(BaseModel):
     repo_id = ForeignKeyField(Repo, backref='pulls')
@@ -28,6 +33,7 @@ class Pull(BaseModel):
     is_merged = BooleanField()
     additions = IntegerField()
     deletions = IntegerField()
+
 
     def serialize(self):
         pull_dict = model_to_dict(self)
