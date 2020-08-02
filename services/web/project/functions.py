02c456ea3f96aaf012431ee1527e52492835c5f4
sapinspys <garri009@fiu.edu>
2020-08-02 01:13:47 -0400

    Added two new functions

--- services/web/project/functions.py
+++ services/web/project/functions.py
@@ -1,24 +1,71 @@
 from github import Github
 import pandas as pd
 
+
 def top_contributors(token, full_name):
-  """Displays top 10 all-time contributors for the given repository.
+    """Return top 10 all-time contributors for the given repository.
 
-  Keywork arguments:
-  full_name -- owner and name of repository in format: "owner/repo"
-  """
-  g = Github(token)
-  repo = g.get_repo(full_name)
-  stats = repo.get_stats_contributors()[90:]
+    Keywork arguments:
+    token -- GitHub authorization token
+    full_name -- owner and name of repository in format: "owner/repo"
+    """
+    g = Github(token)
+    repo = g.get_repo(full_name)
+    stats = repo.get_stats_contributors()[90:]
 
-  repo_data = {
-      'user': [stat.author.login for stat in stats],
-      'name': [g.get_user(stat.author.login).name for stat in stats],
-      'followers': [g.get_user(stat.author.login).followers for stat in stats],
-      'total_commits': [stat.total for stat in stats],
-  }
+    top_contributors = {
+        'user': [stat.author.login for stat in stats],
+        'name': [g.get_user(stat.author.login).name for stat in stats],
+        'followers': [g.get_user(stat.author.login).followers for stat in stats],
+        'total_commits': [stat.total for stat in stats],
+    }
 
-#   columns = list(repo_data.keys())
-#   df = pd.DataFrame(repo_data, columns=columns)
-  
-  return repo_data
+    return top_contributors
+
+
+def owner_summary(token, full_name):
+    """Returns summary of useful repository owner data for the given repository.
+
+    Keywork arguments:
+    token -- GitHub authorization token
+    full_name -- owner and name of repository in format: "owner/repo"
+    """
+    g = Github(token)
+    owner = g.get_user(full_name.split("/")[0])
+
+    owner_summary = {
+      'avatar_url': owner.avatar_url,
+      'gh_url': owner.url,
+      'name': owner.name,
+      'login': owner.login,
+    }
+
+    return owner_summary
+
+
+def repository_summary():
+    """Returns summary of useful repository owner data for the given repository.
+
+    Keywork arguments:
+    token -- GitHub authorization token
+    full_name -- owner and name of repository in format: "owner/repo"
+    """
+    g = Github(token)
+    repo = g.get_repo(full_name)
+
+    repository_summary = {
+      "watchers": repo.subscribers_count,
+      "stars": repo.watchers_count,
+      "forks": repo.forks_count,
+      'full_name': repo.full_name,
+      'description': repo.description,
+      'homepage_url': repo.homepage,
+      'languages_url': repo.languages_url,
+      "updated": repo.last_modified,
+      "contributors": repo.get_contributors().totalCount,
+      "total_commits": repo.get_commits().totalCount,
+      "open pull_requests": repo.get_pulls().totalCount,
+      "closed_pull_requests": repo.get_pulls(state="closed").totalCount,
+    }
+
+    return repository_summary
